0965418f817e3014571591b0d1771a1426a38c86
Maxim Patsyuk <maxuverv@gmail.com>
2023-02-26 10:38:55 +0400

    Small fix

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,5 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 from .utils import get_page_context
